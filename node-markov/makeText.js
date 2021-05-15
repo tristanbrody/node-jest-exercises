6d9d5830ec2fd4e6eafb905197831d7dd0e04ec0
Tristan <tristandavisbrody@gmail.com>
2021-05-14 19:57:49 -0700

    finished additional study parts of exercise

--- node-markov/makeText.js
+++ node-markov/makeText.js
@@ -1,46 +1,59 @@
 /** Command-line tool to generate Markov text. */
 (async () => {
-const fs = require('fs');
-const axios = require('axios');
-const doAsync = require('doasync');
-const MarkovMachine = require("./markov");
+	const prompt = require('prompt-sync')({ sigint: true });
 
+	const fs = require('fs');
+	const axios = require('axios');
+	const doAsync = require('doasync');
+	const striptags = require('striptags');
 
-async function getFileData(path) {
-	return fs.readFile(path, 'utf8', function (err, data) {
-		if (err) {
-			console.log(err);
-			return;
-		}
-	});
-}
+	const MarkovMachine = require('./markov');
 
-async function getURLData(url) {
-	try {
-		const res = await axios.get(url);
-		return res.data;
-	} catch (err) {
-		console.log(`Error: Request failed: ${err}`);
+	async function getFileData(path) {
+		return fs.readFile(path, 'utf8', function (err, data) {
+			if (err) {
+				console.log(err);
+				return;
+			}
+		});
 	}
-}
 
-const pathArg = process.argv[2];
-let re = /^(www)|(https)/;
+	async function getURLData(url) {
+		try {
+			const res = await axios.get(url);
+			return striptags(res.data.trim());
+		} catch (err) {
+			console.log(`Error: Request failed: ${err}`);
+		}
+	}
 
-let textData = await getData(process.argv[2])
-const machine = new MarkovMachine.MarkovMachine(textData);
-machine.makeChains();
-console.log(machine.makeText());
+	const pathArg = process.argv[2];
+	let re = /^(www)|(https)/;
 
-async function getData(arg){
-    let textData;
-    if (pathArg.match(re)) {
-        textData = await getURLData(pathArg).then(data => data);
-    } else {
-        textData = await doAsync(fs)
-        .readFile(process.argv[2], 'utf8')
-        .then(data => data);
-    }
-    return textData;
-    }
+	let textData = await getData(process.argv[2]);
+	const machine = new MarkovMachine.MarkovMachine(textData);
+	machine.makeChains();
+	console.log('****************');
+	console.log('Press enter to keep generating words. Enter q, ctrl + c or n to quit the script');
+	console.log('****************');
+	for (let val of machine.textGenerator()) {
+		let userPrompt = prompt('');
+		console.log(val);
+		if (['q', 'n'].includes(userPrompt)) {
+			break;
+		} else {
+			continue;
+		}
+	}
+	async function getData(arg) {
+		let textData;
+		if (pathArg.match(re)) {
+			textData = await getURLData(pathArg).then(data => data);
+		} else {
+			textData = await doAsync(fs)
+				.readFile(process.argv[2], 'utf8')
+				.then(data => data);
+		}
+		return textData;
+	}
 })();
--- node-markov/markov.js
+++ node-markov/markov.js
@@ -3,76 +3,91 @@
 const fs = require('fs');
 
 class MarkovMachine {
+	/** build markov machine; read in text.*/
 
-  /** build markov machine; read in text.*/
-
-  constructor(text) {
-    let words = text.split(/[ \r\n]+/);
-    this.words = words.filter(c => c !== "");
-    this.makeChains();
-  }
-
-  /** set markov chains:
-   *
-   *  for text of "the cat in the hat", chains will be
-   *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
-
-  makeChains() {
-    this.chains = {}
-    for(let i = 0; i < this.words.length; i++){
-      let newArr = this.words.filter(function(word, index, array){
-        return array[index-1]===array[i];
-      });
-      if(this.words.length === i+1){
-        newArr.push(null);
-      }
-      this.chains[this.words[i]] = newArr;
-    }
-  }
-
-
-  /** return random text from chains */
-
-  makeText(numWords = 100) {
-    let word = randomProperty(this.chains);
-    let str = word;
-    while(word !== null){
-      if(str.split(' ').length===numWords) break;
-
-      const set = new Set(this.chains[word])
-      const allowedWords = [...set]
-      
-      let newWord = allowedWords[randomNumber(0,allowedWords.length)]
-
-      if(newWord!==null) str += ` ${newWord}`;
-      word = newWord;
-    }
-    return str;
-  }
-}
-
-generateTextFromFile('eggs.txt')
-
-function randomNumber(min, max) {
-		return Math.floor(Math.random() * (max - min) + min);
+	constructor(text) {
+		let words = text.split(/[ \r\n]+/);
+		this.words = words.filter(c => c !== '');
+		this.makeChains();
 	}
 
-function randomProperty (obj) {
-    const keys = Object.keys(obj);
-    return keys[randomNumber(0, keys.length)]
-};
+	/** set markov chains:
+	 *
+	 *  for text of "the cat in the hat", chains will be
+	 *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
+
+	makeChains() {
+		this.chains = {};
+		for (let i = 0; i < this.words.length; i++) {
+			let newArr = this.words.filter(function (word, index, array) {
+				return array[index - 1] === array[i];
+			});
+			if (this.words.length === i + 1) {
+				newArr.push(null);
+			}
+			this.chains[this.words[i]] = newArr;
+		}
+	}
+
+	*textGenerator() {
+		//generate random text one word at a time
+		let word = randomProperty(this.chains);
+		let str = word;
+		while (word !== null) {
+			const set = new Set(this.chains[word]);
+			const allowedWords = [...set];
+
+			let newWord = allowedWords[randomNumber(0, allowedWords.length)];
+
+			if (newWord !== null) str += ` ${newWord}`;
+			word = newWord;
+			yield word;
+		}
+		return str;
+	}
+
+	/** return random text from chains */
+
+	makeText(numWords = 100) {
+		let word = randomProperty(this.chains);
+		let str = word;
+		while (word !== null) {
+			if (str.split(' ').length === numWords) break;
+
+			const set = new Set(this.chains[word]);
+			const allowedWords = [...set];
+
+			let newWord = allowedWords[randomNumber(0, allowedWords.length)];
+
+			if (newWord !== null) str += ` ${newWord}`;
+			word = newWord;
+		}
+		return str;
+	}
+}
+
+generateTextFromFile('eggs.txt');
+
+function randomNumber(min, max) {
+	return Math.floor(Math.random() * (max - min) + min);
+}
+
+function randomProperty(obj) {
+	const keys = Object.keys(obj);
+	return keys[randomNumber(0, keys.length)];
+}
 
 function generateTextFromFile(path) {
-    fs.readFile(path, 'utf8', function (err, data) {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      let machine = new MarkovMachine(data);
-      machine.makeChains();
-    });
+	fs.readFile(path, 'utf8', function (err, data) {
+		if (err) {
+			console.log(err);
+			return;
+		}
+		let machine = new MarkovMachine(data);
+		machine.makeChains();
+	});
 }
 
 const machine = new MarkovMachine('the cat in the hat is in the hat');
 
-module.exports = {markovInstance: machine, MarkovMachine}
+module.exports = { markovInstance: machine, MarkovMachine };
